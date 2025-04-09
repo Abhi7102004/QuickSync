8103f746e55475a90525e7dc09fac025443bb675
Abhi7102004 <abhishekyadav7102004@gmail.com>
2025-04-09 22:34:41 +0530

    Ok

--- server/app.js
+++ server/app.js
@@ -29,7 +29,19 @@ app.use("/api/auth", authRoute);
 app.use("/api/contacts", contactRoute);
 app.use("/api/messages", messageRoute);
 app.use("/api/channels", channelRoute);
-
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://quicksync-1.onrender.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 const server = app.listen(port, () => {
   console.log(`Server is running on Port ${port}`);
 });
