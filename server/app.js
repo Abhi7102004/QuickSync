40c1b0f7054047f952a74f485ab226ef7766c29f
Abhi7102004 <abhishekyadav7102004@gmail.com>
2025-04-09 15:49:12 +0530

    Initial commit

--- server/app.js
+++ server/app.js
@@ -12,11 +12,10 @@ const authRoute = require('./routes/auth-route');
 const contactRoute = require('./routes/contact-route');
 const messageRoute=require('./routes/message-route')
 const channelRoute=require('./routes/channel-route');
-const UserModel = require('./models/user-model');
 
 app.use(cors({
     origin: process.env.ORIGIN, 
-    credentials: true, 
+    // credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], 
 }));
 
